ec9d8368f794d6c7ffd8067617caffda15f92ce3
jaytlin <heyjaylin@gmail.com>
2014-09-19 01:09:18 -0700

    Init hw0.

---
+++ hw0.rb
@@ -0,0 +1,57 @@
+########################################################
+###          FOLLOW THE INSTRUCTIONS BELOW           ###
+########################################################
+
+# PROBLEM 1
+# Given an input string, return whether the string is longer
+# than n
+def problem1(string, n)
+
+  # YOUR CODE HERE
+  return string.length > n
+
+end
+
+# PROBLEM 2
+# Given the input string, return the last n characters
+# of the string. Return the whole string if n is greater
+# than the length of the string
+def problem2(string, n)
+
+  # YOUR CODE HERE
+
+end
+
+# PROBLEM 3
+# Given an array of words, concat the words together with
+# a space between each word and return the resulting string
+def problem3(words)
+
+  # YOUR CODE HERE
+
+end
+
+
+########################################################
+###              TEST CASES ARE BELOW                ###
+###             TO RUN THE TESTS, TYPE:              ###
+###        'ruby hw0.py' INTO YOUR TERMINAL          ###
+########################################################
+###      DO NOT EDIT ANYTHING BELOW THIS LINE        ###
+########################################################
+
+def error(problem, test)
+  puts "Problem " + problem.to_s + 
+  " did not pass test " + test.to_s
+end
+
+puts "Running unit tests..."
+
+puts error(1,1) unless problem1('hi', 1) == true
+puts error(1,2) unless problem1('derp',5) == false
+
+puts error(2,1) unless problem2('helloworld',5) == 'world'
+puts error(2,2) unless problem2('hello',10) == 'hello'
+
+words = ['Wow','I','am','so','good','at','Ruby!']
+puts error(3,1) unless problem3(words) == 'Wow I am so good at Ruby!'
